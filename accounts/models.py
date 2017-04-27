500d9203b8b4b9a49b2659eb108a2ad55ede9fce
yh164 <yh164@duke.edu>
2017-04-27 16:17:57 -0400

    a

--- accounts/models.py
+++ accounts/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class AmountBalance(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="balance")
     balance = models.IntegerField(default=100)
-
+    def __str__(self):
+        return self.user.username + " : " + str(self.balance)
 # Create your models here.
